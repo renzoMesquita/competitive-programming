5e9a814126964aba320d00cc4bc8dff2b84417ab
Renzo <renzomesquita25@gmail.com>
2026-05-29 09:26:28 -0300

    beecrowd - 1435.cpp

---
+++ 29-05-26/beecrowd - 1435.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+using namespace std;
+ 
+int main() {
+ 
+    int n, mat[n][n], i, j, valor;
+    
+    while(true){
+        
+        cin >> n;
+        
+        if(n == 0){
+            break;
+        }
+        
+        for(i=0; i<n; i++){
+           
+            for(j=0; j<n; j++){
+                
+                valor = min(min(i, j), min(n-1-i, n-1-j)) + 1;
+                
+                cout << setw(3) << valor;
+                
+                if(j != n-1){
+                    cout << " ";
+                }
+            }
+            
+            cout << endl;
+        }
+        
+        cout<< endl;
+    }
+ 
+    return 0;
+}

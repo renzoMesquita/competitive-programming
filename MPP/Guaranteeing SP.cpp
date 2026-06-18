ddba17486616cd6669bda0bcfe92e6bab4795244
Renzo <renzomesquita25@gmail.com>
2026-06-18 09:50:11 -0300

    Guaranteeing SP.cpp

---
+++ MPP/Guaranteeing SP.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    string cod;
+
+    while(cod.size() != 5){
+        cin >> cod;
+    }
+
+    if(cod[3] == 'S'){
+        if(cod[4] == 'P'){
+            cout << 'S' << endl;
+        }
+        else{
+            cout << 'T' << endl;
+        }
+    }
+    else if(cod[4] == 'P'){
+        cout << "T" << endl;
+    }
+    else{
+        cout << "N" << endl;
+    }
+
+    return 0;
+    
+}
